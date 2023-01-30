d7f1dff0797a5f9ff3f91d6b5dc3a5555528489d
ymind14563 <ymind14563@gmail.com>
2023-01-30 10:43:58 +0900

    데일리코딩 8번 문제

--- src/Main.java
+++ src/Main.java
@@ -1,11 +1,19 @@
+import java.util.Arrays;
 
 public class Main {
     public static void main(String[] args) {
 
-        convertDoubleSpaceToSingle convertDoubleSpaceToSingle = new convertDoubleSpaceToSingle();
+//        convertDoubleSpaceToSingle convertDoubleSpaceToSingle = new convertDoubleSpaceToSingle();
+//
+//        String output = convertDoubleSpaceToSingle.convertDoubleSpaceToSingle("string  with  double  spaces");
+//        System.out.println(output); // --> "string with double spaces"
 
-        String output = convertDoubleSpaceToSingle.convertDoubleSpaceToSingle("string  with  double  spaces");
-        System.out.println(output); // --> "string with double spaces"
+        removeExtremes removeExtremes = new removeExtremes();
 
+        String[] output = removeExtremes.removeExtremes(new String[]{"where", "is", "the", "longest", "word"});
+        System.out.println(Arrays.toString(output)); // --> ["where", "the", "word"]
+        //Arrays.toString(output); // --> ["where", "the", "word"]
+
+        // 배열을 출력할땐 System.out.println(Arrays.toString(output)); Arrays.toString 없으면 주소값이 출력된다.
     }
 }
---
+++ src/removeExtremes.java
@@ -0,0 +1,40 @@
+import java.util.*;
+
+public class removeExtremes {
+     	public String[] removeExtremes(String[] arr) {
+     // TODO:
+ 		if (arr.length == 0) return null;
+
+ 		//빈배열 생성
+ 		//초기값(=비교값) 에 첫번째 arr 배열요소 글자수 입력
+ 		//비교하여(같은 것도 포함해야 제일 나중위치가 고려됨)
+ 		//비교값에 해당 배열 요소 넣어주고,
+ 		//짧은 배열요소, 긴배열 요소 의 인덱스값 입력
+ 		//짧은, 긴 인덱스 값이 아니면 빈배열에 넣어 주기
+
+ 		String[] result = new String[arr.length - 2];
+ 		int compare_arr_long = arr[0].length();
+		int compare_arr_short = arr[0].length();
+ 		int longIndex = 0;
+ 		int shortIndex = 0;
+
+ 		for (int i = 1; i < arr.length; i++){
+ 			if (compare_arr_long <= arr[i].length()){
+ 			compare_arr_long = arr[i].length();
+ 			longIndex = i;
+ 			}
+ 			if (compare_arr_short >= arr[i].length()){
+ 			compare_arr_short = arr[i].length();
+ 			shortIndex = i;
+ 			}
+ 		}
+ 		int j = 0;
+ 		for (int i = 0; i < arr.length; i++){
+ 			if (i != longIndex && i != shortIndex){
+ 				result[j] = arr[i];
+ 				j++;
+ 			}
+ 		}
+ 		return result;
+ 	}
+}
