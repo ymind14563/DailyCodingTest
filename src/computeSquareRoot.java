4bbe37472b5435f0b6ce10075071085c3dec76d0
ymind14563 <ymind14563@gmail.com>
2023-02-10 22:28:43 +0900

    데일리코딩 17번 문제

---
+++ src/computeSquareRoot.java
@@ -0,0 +1,12 @@
+public class computeSquareRoot {
+    public static String computeSquareRoot(int num) {
+        // TODO:
+        //바빌로니아 공식 (제곱근 구하는 공식)
+        double result = num; // 범위를 크게 벗어나지 않으면 어떤 수(num)도 상관없음.
+        for (int i = 0 ; i < num ; i++) {
+            result = 0.5 * (num / result + result);
+        }
+
+        return String.format("%.2f", result);
+    }
+}
