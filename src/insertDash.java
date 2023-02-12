36e0439b956be9483c6ea0391275319e5fbbb121
ymind14563 <ymind14563@gmail.com>
2023-02-12 17:58:32 +0900

    데일리코딩 10번 문제

--- src/insertDash.java
+++ src/insertDash.java
@@ -1,14 +1,14 @@
-public class DailyCoding {
+public class insertDash {
     public String insertDash(String str) {
         // TODO:
-        if(str.length()==0) return null;
+        if (str.length() == 0) return null;
 
         String result = "" + str.charAt(0); //String에 char만 넣으면 변환이 안돼서 오류남. //""// 있어야 됨.
 
-        for(int i = 1; i < str.length(); i++) {
+        for (int i = 1; i < str.length(); i++) {
             int firstnum = Character.getNumericValue(str.charAt(i - 1)) % 2; // char -> int로 변환 Character.getNumericValue
             int nextnum = Character.getNumericValue(str.charAt(i)) % 2;
-            if(firstnum != 0 && nextnum != 0) {
+            if (firstnum != 0 && nextnum != 0) {
                 result = result + "-";
             }
             result = result + str.charAt(i); //이미 초기화할때 //""// 들어있기에 char만 넣어도 오류 나지 않음.
@@ -16,5 +16,13 @@ public class DailyCoding {
         return result;
 
     }
+
+    public static void main(String[] args) {
+
+        insertDash insertDash = new insertDash();
+        String output = insertDash.insertDash("454793");
+        System.out.println(output); // --> 4547-9-3
+    }
+
 }
 
